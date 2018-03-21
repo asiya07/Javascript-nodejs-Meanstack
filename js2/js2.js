e8002795dff0d017462a538e06fe1b04c29e2f89
asiya <mujawarasiya7@gmail.com>
2018-03-21 18:47:00 +0530

    This is JS2 Example

---
+++ js2/js2.js
@@ -0,0 +1,25 @@
+/*
+ * Filename: d:\js2\js2.js
+ * Path: d:\js2
+ * Created Date: Wednesday, March 21st 2018, 6:28:07 pm
+ * Author: Asiya
+ * 
+ * Copyright (c) 2018 Your Company
+ */
+
+
+var express = require("express");
+var app = express();
+ var port= 3000;
+
+ app.use('/' ,(req,res)=>
+ {
+     //res.send("hello");
+     res.sendFile(__dirname +"/index.html")
+ });
+
+ app.listen(port,()=>{
+
+     console.log("listening at"+port);
+ });
+ 
